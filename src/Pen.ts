89ff9492f6aeb723bb245174e08a0aabc5ac1b31
Ben Clark <bencoder@googlemail.com>
2015-07-25 20:19:05 +0100

    add pen

---
+++ src/Pen.ts
@@ -0,0 +1,15 @@
+interface PolarCoordinate {
+    angle : number;
+    distance : number;
+}
+
+class Pen {
+    public parent : Shape;
+    public position : PolarCoordinate
+
+
+    getWorldLocation()
+    {
+
+    }
+}
--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,6 @@
+/// <reference path="Pen.ts"/>
+/// <reference path="Shape.ts"/>
+
 var canvas : HTMLCanvasElement = document.createElement("canvas");
 canvas.style.width = "100%";
 canvas.style.height = "100%";
@@ -10,3 +13,8 @@ var context : CanvasRenderingContext2D = canvas.getContext("2d");
 context.beginPath();
 context.arc(canvas.width/2,canvas.height/2,Math.min(canvas.width,canvas.height)/2 - 10,0,2*Math.PI,false);
 context.stroke();
+
+
+
+var pen = new Pen();
+pen.getWorldLocation();
